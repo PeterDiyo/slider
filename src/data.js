ac2f4d4e032ee59fa5f8914b72dd76028b175250
unknown <PeterDiyo>
2024-03-30 09:26:56 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,126 @@
+export const shortList = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+];
+
+export const list = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.",
+  },
+  {
+    id: 2,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.",
+  },
+  {
+    id: 3,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.",
+  },
+  {
+    id: 4,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ",
+  },
+];
+
+export const longList = [
+  {
+    id: 1,
+    image:
+      "https://scontent.fmaa12-2.fna.fbcdn.net/v/t39.30808-6/428680977_1564656100955709_8385597311023653717_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=11UXRTS3fPYAX_k-YQt&_nc_ht=scontent.fmaa12-2.fna&cb_e2o_trans=q&oh=00_AfDCjxAWUwYrFpTBCE_8cuoy4B3tRL_gO25jfrfScac4aw&oe=660B23EE",
+    name: "Diyo Peter",
+    title: "Software Engineer",
+    quote:
+      "Hi, I am a Software engineer. Software Engineering is the process of designing, developing, testing, and maintaining software. It is a systematic and disciplined approach to software development that aims to create high-quality, reliable, and maintainable software systems",
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fmaa12-3.fna.fbcdn.net/v/t39.30808-6/383237046_328801936341033_2935473463390485703_n.jpg?stp=c0.11.206.206a_dst-jpg_p206x206&_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=U3wJx_cyPh8AX-gHnec&_nc_ht=scontent.fmaa12-3.fna&cb_e2o_trans=q&oh=00_AfA-bwYUPgKtVLWo0jvgyvsMjM-ZljYhTwcuEcmCl7V_OQ&oe=660C1A23",
+    name: "Le Boss",
+    title: "Developer",
+
+    quote:
+      "Software Development is defined as the process of designing, creating, testing, and maintaining computer programs and applications. The developer does software decelopment for the users interface and user experince. Godwin Le Boss, Godwin Le Boss Godwin Le Boss Godwin Le ",
+  },
+  {
+    id: 3,
+    image:
+      "https://scontent.fmaa12-2.fna.fbcdn.net/v/t39.30808-6/279206327_1784424528428143_3527039606702667406_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QjzkzJt3N1YAX8srzOw&_nc_ht=scontent.fmaa12-2.fna&cb_e2o_trans=q&oh=00_AfAHSAvlPZ7jaAACBsQ3O3UHgHkMCujjUEcXfKfjaf9k9Q&oe=660A8E39",
+    name: "Riiny T.B",
+    title: "Optometrist",
+
+    quote:
+      "An optometrist is a healthcare professional who provides primary vision care. He/she is licensed to practice optometry, which includes giving eye exams, writing prescriptions, and diagnosing and treating some eye conditions and diagnosing and treating some eye conditions",
+  },
+  {
+    id: 4,
+    image:
+      "https://scontent.fmaa12-1.fna.fbcdn.net/v/t39.30808-6/328632252_673751167836375_7446458029967276318_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=IwDMfH_WArUAX9_88rj&_nc_ht=scontent.fmaa12-1.fna&cb_e2o_trans=q&oh=00_AfCewx5rdsHESGq-6evy4yiEh1r9J5st1t4NkHlNDHl2UQ&oe=660ABBC7",
+    name: "Lawrence M",
+    title: "Business analyst",
+
+    quote:
+      "A business analyst is a person who analyzes and assesses the current business processes, products, services, and systems using data analytics tools and approaches A business analyst is a person who analyzes and assesses the current business processes, products, services",
+  },
+  {
+    id: 1,
+    image:
+      "https://scontent.fmaa12-2.fna.fbcdn.net/v/t39.30808-6/428680977_1564656100955709_8385597311023653717_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=11UXRTS3fPYAX_k-YQt&_nc_ht=scontent.fmaa12-2.fna&cb_e2o_trans=q&oh=00_AfDCjxAWUwYrFpTBCE_8cuoy4B3tRL_gO25jfrfScac4aw&oe=660B23EE",
+    name: "Diyo Peter",
+    title: "Software Engineer",
+    quote:
+      "Hi, I am a Software engineer. Software Engineering is the process of designing, developing, testing, and maintaining software. It is a systematic and disciplined approach to software development that aims to create high-quality, reliable, and maintainable software systems",
+  },
+  {
+    id: 2,
+    image:
+      "https://scontent.fmaa12-3.fna.fbcdn.net/v/t39.30808-6/383237046_328801936341033_2935473463390485703_n.jpg?stp=c0.11.206.206a_dst-jpg_p206x206&_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=U3wJx_cyPh8AX-gHnec&_nc_ht=scontent.fmaa12-3.fna&cb_e2o_trans=q&oh=00_AfA-bwYUPgKtVLWo0jvgyvsMjM-ZljYhTwcuEcmCl7V_OQ&oe=660C1A23",
+    name: "Le Boss",
+    title: "Developer",
+
+    quote:
+      "Software Development is defined as the process of designing, creating, testing, and maintaining computer programs and applications. The developer does software decelopment for the users interface and user experince. Godwin Le Boss, Godwin Le Boss Godwin Le Boss Godwin Le ",
+  },
+  {
+    id: 3,
+    image:
+      "https://scontent.fmaa12-2.fna.fbcdn.net/v/t39.30808-6/279206327_1784424528428143_3527039606702667406_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QjzkzJt3N1YAX8srzOw&_nc_ht=scontent.fmaa12-2.fna&cb_e2o_trans=q&oh=00_AfAHSAvlPZ7jaAACBsQ3O3UHgHkMCujjUEcXfKfjaf9k9Q&oe=660A8E39",
+    name: "Riiny T.B",
+    title: "Optometrist",
+
+    quote:
+      "An optometrist is a healthcare professional who provides primary vision care. He/she is licensed to practice optometry, which includes giving eye exams, writing prescriptions, and diagnosing and treating some eye conditions and diagnosing and treating some eye conditions",
+  },
+  {
+    id: 4,
+    image:
+      "https://scontent.fmaa12-1.fna.fbcdn.net/v/t39.30808-6/328632252_673751167836375_7446458029967276318_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=IwDMfH_WArUAX9_88rj&_nc_ht=scontent.fmaa12-1.fna&cb_e2o_trans=q&oh=00_AfCewx5rdsHESGq-6evy4yiEh1r9J5st1t4NkHlNDHl2UQ&oe=660ABBC7",
+    name: "Lawrence M",
+    title: "Business analyst",
+
+    quote:
+      "A business analyst is a person who analyzes and assesses the current business processes, products, services, and systems using data analytics tools and approaches A business analyst is a person who analyzes and assesses the current business processes, products, services",
+  },
+];
